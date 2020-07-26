2999df4fe98351e77e1659d1ee834fb2bc98facd
daisuke8000 <daisuke12108000@gmail.com>
2020-07-26 09:00:17 +0900

    new,show,edit routes

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -15,6 +16,9 @@ var e = createMux()
 func main() {
 	// Routes
 	e.GET("/", articleIndex)
+	e.GET("/new", articleNew)
+	e.GET("/:id", articleShow)
+	e.GET("/:id/edit", articleEdit)
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
 }
@@ -33,15 +37,47 @@ func createMux() *echo.Echo {
 	return e
 }
 
-// Hundler
+// IndexHundler
 func articleIndex(c echo.Context) error {
-	//return c.String(http.StatusOK, "Hello, Would!")
 	data := map[string]interface{}{
-		"Message": "Hello, World!",
+		"Message": "Article Index",
 		"Now":     time.Now(),
 	}
 	return render(c, "article/index.html", data)
 }
+
+// NewHundler
+func articleNew(c echo.Context) error {
+	data := map[string]interface{}{
+		"Message": "Article New",
+		"Now":     time.Now(),
+	}
+	return render(c, "article/new.html", data)
+}
+
+// ShowHundler
+func articleShow(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	data := map[string]interface{}{
+		"Message": "Article Show",
+		"Now":     time.Now(),
+		"ID": id,
+	}
+	return render(c, "article/show.html", data)
+}
+
+// EditHundler
+func articleEdit(c echo.Context) error {
+	//c.Paramで取り出した値は文字列型になるため、ここでは数値型にキャストしている。
+	id, _ := strconv.Atoi(c.Param("id"))
+	data := map[string]interface{}{
+		"Message": "Article Edit",
+		"Now":     time.Now(),
+		"ID": id,
+	}
+	return render(c, "article/edit.html", data)
+}
+
 
 func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
 	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
